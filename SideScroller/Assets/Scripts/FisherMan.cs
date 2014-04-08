1d7f06947c527953fe8c6bbc5d8393faa36658d8
JulieZintel <jzinte12@student.aau.dk>
2014-04-08 13:01:14 +0200

    Update 3.1

--- SideScroller/Assets/Scripts/FisherMan.cs
+++ SideScroller/Assets/Scripts/FisherMan.cs
@@ -3,47 +3,45 @@ using UnityEngine;
 
 public class FisherMan : MonoBehaviour {
 
-	public float speed = 10.0f; // The speed of the hero, when it moves in the plane
-	public static float score = 0;
-	public Transform exp;	
-	public Vector3 jumpVelocity;
+	public int speed = 20;
+	public float jumpSpeed = 13.0f;
+	public float gravity = 0.0f;
 
-	void Start () {
-	
+
+	private Vector2 moveDirection = Vector2.zero;
+	private CharacterController fisherMan;
+
+	void Start (){
+		fisherMan = GetComponent<CharacterController> ();
+		if (!fisherMan)
+			fisherMan = gameObject.AddComponent<CharacterController> ();
+
 	}
-	
-	// Update is called once per frame
 	void Update () {
+	
 
-		rigidbody2D.velocity = new Vector2(7,0);
+		if (fisherMan.isGrounded) {
+			moveDirection = new Vector2 (Input.GetAxis ("Horizontal"), 0);
+			moveDirection = transform.TransformDirection (moveDirection);
+			moveDirection *= speed;
 
-		// Making movements options for my avatar - A for left, D for right, space for jump and S for duck and W for wide
-
-		if(touchingPlatform && Input.GetButtonDown("Jump")){
-			rigidbody.AddForce(jumpVelocity, ForceMode.VelocityChange);
+			if (Input.GetKeyDown ("Jump")) {
+				moveDirection.y = jumpSpeed;
+			}
+			if (Input.GetKeyDown ("Horizontal")) {
+				speed = 30;
+				jumpSpeed = 15.0f;
+			}
 		}
-		distanceTraveled = transform.localPosition.x;
-	/*			if(Input.GetKeyDown(KeyCode.A)){
-				rigidbody.AddForce(100.0f, 0, 0);
-		}
-		if(Input.GetKeyDown(KeyCode.Space)){
-			//		rigidbody.AddForce(0, 400.0f, 0);
-		} */
 
+		if (Input.GetKeyUp ("Horizontal")) {
+			speed = 20;
+			jumpSpeed = 13.0f;
+		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		fisherMan.Move (moveDirection * Time.deltaTime);
 	}
-
-	/*	void OnCollisionEnter(Collision other){
-		if (other.gameObject.name == "Shark"){
-
-			score = score - 10;
-			Instantiate (exp, transform.position, Quaternion.identity);
-			Destroy(gameObject);
-			// a function which makes the speed of R2D2 slower
-
-		}else if(other.gameObject.name == "Fish"){
-			score = score + 10;
-			Instantiate (exp, transform.position, Quaternion.identity);
-			Destroy(gameObject);
-		}
-	}*/
+	void Move(){
+		moveDirection = transform.position.x + speed;
+	}
 }
