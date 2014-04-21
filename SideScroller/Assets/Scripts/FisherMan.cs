5265b88a34d40cd13517dd721ed2f50b393b73c2
JulieZintel <jzinte12@student.aau.dk>
2014-04-21 15:35:28 +0200

    Hero
    I’ve now made a Start scene, build it with our Main scene, to be the
    new ‘Gameplay’, where you can try out the game..
    
    I’ve also made the camera as child to the Fisherman, so it follows only
    with the x-axis and not with the jump, so it is possible to se if it’s
    hitting other obstacles
    
    At last I made the jump function! Yaaay!
    
    I also deleted the Maincamera.cs-script, it will not be needed anymore

--- SideScroller/Assets/Scripts/FisherMan.cs
+++ SideScroller/Assets/Scripts/FisherMan.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class FisherMan : MonoBehaviour {
-
+	private GameObject cameraRef;
 	public float speed = 0.0f; // The speed of the hero, when it moves in the plane
 	public static float score = 0;
 	public Transform exp;
@@ -17,6 +17,8 @@ public class FisherMan : MonoBehaviour {
 	public double ground_y = -1.136831;
 
 	void Start () {
+		cameraRef = GameObject.Find("MainCamera");
+
 		boosttimer = boosttime;
 		if (transform.position.y < ground_y && !isGrounded) {
 			isGrounded = true;
@@ -26,20 +28,13 @@ public class FisherMan : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
+		cameraRef.transform.position = new Vector3(transform.position.x+5f, 1.6f, -10f);
 
-
-
-		/*if (Input.GetKeyDown (KeyCode.D)) {
-			direction_right = true;
-		}
-		if (Input.GetKeyDown (KeyCode.A)) {
-			direction_left = true;
-		}*/
 		if (Input.GetKeyDown (KeyCode.D) && speed <15.0f) {
 
 			speed += 1.0f;
 		}
-		if (Input.GetKeyDown (KeyCode.D) && speed < -15.0f) {
+		if (Input.GetKeyDown (KeyCode.A) && speed < -15.0f) {
 
 			speed -= 1.0f;
 		}
@@ -53,43 +48,14 @@ public class FisherMan : MonoBehaviour {
 			boosttimer = boosttime;
 		}
 
-		/*if (direction_right == true && boost == false) {
-			rigidbody2D.AddForce (transform.right * speed);
-			direction_right = false;
-		} else if (direction_right == true && boost == true) {
-			rigidbody2D.AddForce (transform.right * speed);
-			direction_right = false;
-		}
-<<<<<<< HEAD
-
-		if (direction_left == true && boost == false) {
-			rigidbody2D.AddForce (-transform.right * speed);
-			direction_left = false;
-		} else if (direction_left == true && boost == true) {
-			rigidbody2D.AddForce (-transform.right * speed);
-			direction_left = false;
-		}*/
-		/*	if (Input.GetKeyDown (KeyCode.W) && transform.position.y < (-7.0f) ) {
-		//	rigidbody2D.velocity = new Vector2 (0, 5.0f);
-
-		rigidbody2D.AddForce(transform.up*100*Jump);
-	}*/
-	/*	if(isGrounded && Input.GetKey(KeyCode.W)) {
-	if (transform.position.y > 0) {
-		rigidbody2D.AddForce (transform.up*100 * Jump);
-	}}*/
-/// <summary>
-/// reset y pos til ground, y-vel til 0
-/// </summary>
 if (isGrounded && Input.GetKey (KeyCode.W)) {
 	isGrounded = false;
-	//rigidbody2D.AddForce (transform.up * 5 * Jump);
-	Jump = 5.0f;
+			Jump = 20.0f;
 
-} else if (transform.position.y > ground_y && Jump > -5.0f  ) {
-	Jump -= 1.0f;	
+		} else if (transform.position.y > ground_y && Jump > -20.0f  ) {
+			Jump -= 0.5f;	
 	transform.rigidbody2D.velocity = new Vector2 (speed, Jump);	
-	//transform.rigidbody2D.AddForce (transform.up * -5);
+
 } else if (transform.position.y <= ground_y && !isGrounded){
 	Jump = 0.0f;
 	transform.rigidbody2D.velocity = new Vector2 (speed, Jump);
---
+++ SideScroller/Assets/Scripts/Start.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Start : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Q)){
+			Application.LoadLevel("Main");
+
+		}
+	}
+}
